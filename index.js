6e17105d09c38bcc6520e776483bd7d46f38f90d
Polina <lady.polina1997@yandex.ru>
2023-07-11 21:28:43 +0300

    InsertionSort

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const {StasSort} = require('./sorts/StasSort')
 const {bugusSort} = require('./sorts/bogusSort');
 const {bubbleSort}=require('./sorts/bubbleSort');
 const {GnomeSort}=require('./sorts/GnomeSort');
+const {InsertionSort}=require('./sorts/InsertionSort');
 let a=getRandomArray(100,0,1000);
 //let b=StasSort (a);
 //let b=bugusSort (a);
-let b=GnomeSort (a);
+//let b=GnomeSort (a);
+let b=InsertionSort (a);
 console.log (b);
 console.log (checkArraySort (b));
 //console.log (getRandomArray (3));
